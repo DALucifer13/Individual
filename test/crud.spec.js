9cd01635ba4f81b63c30cd82dfa7002e03670674
Kyle <xkylemcx13@gmail.com>
2019-11-19 09:32:49 +0000

    msg

--- test/crud.spec.js
+++ test/crud.spec.js
@@ -7,9 +7,53 @@ chai.use(chaihttp);
 describe("Music", () => {
     it("create", (done) => {
         chai.request(app)
-            .post('http://localhost:1024/home/music/add/save')
+            .post('/home/music/add/save')
             .type('json')
             .send({ name: "Zelda Main Theme Marimba", image: "pic.jpg", album: "Zelda", artist: "Zelda", genre: "Game", length: "4:00", location: "/home/music/ZeldaMtm.mp3" })
+            .end((err, res) => {
+                chai.expect(res.status).to.eq(200);
+                done();
+            })
+    });
+
+    it("read", (done) => {
+        chai.request(app)
+            .get("/home/music/search/select/id/1")
+            .end((err, res) => {
+                chai.expect(res.status).to.eq(200);
+                chai.expect(res.body[0].id).to.eq(1);
+                chai.expect(res.body[0].name).to.eq("Zelda Main Theme Marimba");
+                chai.expect(res.body[0].genre).to.eq("Game");
+                done();
+            })
+    });
+
+    it("update", (done) => {
+        chai.request(app)
+            .put('/home/music/edit/save')
+            .type('json')
+            .send({ id: 1, name: "Zelda Main Theme Marimba", image: "pic.jpg", album: "Zelda", artist: "Zelda", genre: "Orchestra", length: "4:00", location: "/home/music/ZeldaMtm.mp3" })
+            .end((err, res) => {
+                chai.expect(res.status).to.eq(200);
+                done();
+            })
+    });
+
+    it("readupdate", (done) => {
+        chai.request(app)
+            .get("/home/music/search/select/id/1")
+            .end((err, res) => {
+                chai.expect(res.status).to.eq(200);
+                chai.expect(res.body[0].id).to.eq(1);
+                chai.expect(res.body[0].name).to.eq("Zelda Main Theme Marimba");
+                chai.expect(res.body[0].genre).to.eq("Orchestra");
+                done();
+            })
+    });
+
+    it("delete", (done) => {
+        chai.request(app)
+            .delete('/home/music/view/delete/1')
             .end((err, res) => {
                 chai.expect(res.status).to.eq(200);
                 done();
